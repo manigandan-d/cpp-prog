ece96b3b35f3c36be92c89f0a5dfde0ea1e0826c
manigandan-d <manigandan220916@gmail.com>
2024-03-16 18:26:33 +0530

    data type

--- Exercises/Data Type/main.cpp
+++ Exercises/Data Type/main.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+/*
+Integer: 4 bytes
+Long: 8 bytes
+Float: 4 bytes
+Double: 8 bytes
+Character: 1 byte
+
+You’re given a data type. Print its size in bytes.
+*/
+
 int dataTypes(string type) {
 	if(type == "Integer")
 		return 4;
@@ -25,3 +35,11 @@ int main() {
 
 	cout << dataTypes(data) << endl;
 }
+
+/*
+Sample Input 1 
+Long
+
+Sample Input 2 
+Float
+*/
