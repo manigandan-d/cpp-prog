5460408296d04e65dbe95e980817c2b82b4c6a3b
manigandan-d <manigandan220916@gmail.com>
2024-03-16 16:15:10 +0530

    comments

---
+++ Tutorial/Fundamentals/Comments/comments.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // cout << "/*";
+    // cout << "*/";
+    // cout << /* "*/" */;
+    cout << /* "*/" /* "/*" */;
+    int a = /* 10 */ 10;
+    cout << a ;
+
+    return 0;
+}
