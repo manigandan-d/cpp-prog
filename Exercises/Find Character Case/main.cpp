eddbc77173fb719e877eca57671d3e18d173aa3d
manigandan-d <manigandan220916@gmail.com>
2024-03-16 18:27:54 +0530

    find the character case

--- Exercises/Find Character Case/main.cpp
+++ Exercises/Find Character Case/main.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+/* 
+Write a program that takes a character as input and prints 1, 0, or -1 according to the following rules.
+1, if the character is an uppercase alphabet (A - Z).
+0, if the character is a lowercase alphabet (a - z).
+-1, if the character is not an alphabet.
+*/
+
 int main() {
 	char ch;
 	cin >> ch;
@@ -17,3 +24,14 @@ int main() {
 	}
 	
 }
+
+/*
+Sample Input 1
+v
+
+Sample Input 2 
+D
+
+Sample Input 3 
+# 
+*/
