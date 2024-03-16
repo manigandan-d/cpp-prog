cc9ada1f05b8758cd2322c1f4ee1508a29ee4bbc
manigandan-d <manigandan220916@gmail.com>
2024-03-16 17:19:07 +0530

    string

---
+++ Tutorial/String/string.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // string name;
+    // cin >> name;
+    // cout << name << endl;
+
+    string info;
+    getline(cin, info);
+    cout << info << endl;
+
+    return 0;
+}
