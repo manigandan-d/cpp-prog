942fe355c64f7527c6fcbe08c2b26e3b69e6a239
manigandan-d <manigandan220916@gmail.com>
2024-03-17 11:30:08 +0530

    updated

---
+++ Tutorial/Array/array.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n = 5;
+
+int main() {
+    int arr[n];
+
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
