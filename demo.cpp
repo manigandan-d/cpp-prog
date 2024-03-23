388ce85cae31a5ed34dfa0f5b4b2e3752186b3c0
manigandan-d <manigandan220916@gmail.com>
2024-03-23 15:47:11 +0530

    updated

--- demo.cpp
+++ demo.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
-#include <list>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
-    list<int> ls;
-
-    ls.push_back(5);
-    ls.emplace_back(7);
-
-    ls.push_front(3);
-    ls.emplace_front(1);
-
-    for(auto i: ls) {
+void display(vector<int> vect) {
+    for(auto i: vect) {
         cout << i << " ";
     }
     cout << endl;
+}
+
+int main() {
+    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    display(v);
+
+    vector<int> :: iterator it = v.begin();
+
+    while(it != v.end()) {
+        if(*it % 2 == 0) {
+            v.erase(it);
+        }
+
+        else {
+            it++;
+        }
+    }
+
+    display(v);
 
     return 0;
 }
